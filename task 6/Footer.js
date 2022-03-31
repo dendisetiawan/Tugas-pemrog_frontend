593b7ff0e0774a7a9bdc9f1edbce2adb61ad5e8b
dendisetiawan <55815656+dendisetiawan@users.noreply.github.com>
2022-03-31 22:40:46 +0700

    Add files via upload

---
+++ task 6/Footer.js
@@ -0,0 +1,17 @@
+
+ import styles from "./Footer.module.css";
+
+ function Footer() {
+ 
+   return (
+     <div className={styles.container}>
+       <footer className={styles.footer}>
+         <h2 className={styles.footer__title}>Movie App</h2>
+         <p className={styles.footer__author}>Created by dendi</p>
+       </footer>
+     </div>
+   );
+ }
+ 
+ export default Footer;
+ 
---
+++ task 6/main.js
@@ -0,0 +1,16 @@
+import Hello from "./Hello";
+
+function Main() {
+  return (
+    <main>
+      {}
+      <Hello name="Aufa" />
+      <Hello name="Mikel" />
+      <Hello name="Hannah" />
+      <Hello name="Jonas" />
+      <Hello name="Martha" />
+    </main>
+  );
+}
+
+export default Main;
